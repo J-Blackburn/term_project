d3c0e9ef1bf7564840c6215caddaf291ff7f2490
J-Blackburn <J-Blackburn@users.noreply.github.com>
2018-04-16 22:54:15 -0400

    Update circuitDescriptionParsing.cpp

--- circuitDescriptionParsing.cpp
+++ circuitDescriptionParsing.cpp
@@ -1,25 +1,82 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+
+#include "gate.h"
+#include "wire.h"
+
 using namespace std;
 
-int main() {
-	string test = "ONE  TWO   THREE";
-	string test2 = "First  Second";
-	string a,b,c,d;
-	a = test.substr(0, test.find(" "));
-	test.erase(0,test.find(" "));
-	test.erase(0,test.find_first_not_of(" "));
-	b = test.substr(0,test.find(" "));
-	test.erase(0,test.find(" "));
-	test.erase(0,test.find_first_not_of(" "));
-	c = test.substr(0,test.find(" "));
-	cout << a << "." << b << "." << c << "." << endl;
-	a = test2.substr(0, test2.find(" "));
-	test2.erase(0,test2.find(" "));
-	test2.erase(0,test2.find_first_not_of(" "));
-	b = test2.substr(0,test2.find(" "));
-	test2.erase(0,test2.find(" "));
-	test2.erase(0,test2.find_first_not_of(" "));
-	c = test2.substr(0,test2.find(" "));
-	cout << a << "." << b << "." << c << "." << endl;
+void wireInitialization(string ln) {
+	string wireType, wireName;
+	int wireNumber;
+	wireType = ln.substr(0, ln.find(" "));
+	ln.erase(0,ln.find(" "));
+	ln.erase(0,ln.find_first_not_of(" "));
+	wireName = ln.substr(0,ln.find(" "));
+	ln.erase(0,ln.find(" "));
+	ln.erase(0,ln.find_first_not_of(" "));
+	wireNumber = stoi(ln.substr(0,ln.find(" ")));
+	cout << "Type: " << wireType << " Name: " << wireName;
+	cout << " Number: " << wireNumber << endl;
+	//create wire
+	wire myWire(wireName, wireNumber);
+}
+
+void gateInitialization(string ln) {
+	string gateType, gd;
+	int gateDelay, in1, in2, out;
+	gateType = ln.substr(0, ln.find(" "));
+	ln.erase(0,ln.find(" "));
+	ln.erase(0,ln.find_first_not_of(" "));
+	gd = ln.substr(0,ln.find(" "));
+	gd.erase(gd.find("ns"), gd.size());
+	gateDelay = stoi(gd);
+	ln.erase(0,ln.find(" "));
+	ln.erase(0,ln.find_first_not_of(" "));
+	in1 = stoi(ln.substr(0,ln.find(" ")));
+	ln.erase(0,ln.find(" "));
+	ln.erase(0,ln.find_first_not_of(" "));
+	in2 = stoi(ln.substr(0,ln.find(" ")));
+	ln.erase(0,ln.find(" "));
+	ln.erase(0,ln.find_first_not_of(" "));
+	string temp = ln.substr(0,ln.find(" "));
+	if(temp == "") {
+		out = in2;
+		in2 = -1;
+	}
+	else {
+		out = stoi(temp);
+	}
+	cout << "Type: " << gateType << " Delay: " << gateDelay;
+	cout << " Input 1: " << in1 << " Input 2: " << in2 ;
+	cout << " Output: " << out << endl;
+	//create gate
+	gate myGate(gateType, /*name, wire* in1, wire* in2, wire* out,*/gateDelay)
+}
+
+bool importFile(char *input) {
+	string line;
+	ifstream myfile (input);
+	if(myfile.is_open()) {
+		while(getline(myfile,line)) {
+			if(line.substr(0,line.find(" ")) == "CIRCUIT")
+				cout << "File is correct" << endl;
+			else if(line.substr(0,line.find(" ")) == "INPUT" || line.substr(0,line.find(" ")) == "OUTPUT")
+				wireInitialization(line);
+			else
+				gateInitialization(line);
+		}
+    myfile.close();
+	}
+	else return 0;;
+	return 1;
+}
+
+int main(int argc, char **argv) {
+	if(importFile(argv[1]))
+		cout << "SUCCESSFUL PARSING" << endl;
+	else 
+		cout << "FAILED PARSING" << endl;
+	return 0;
 }
