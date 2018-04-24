ff8a998007de127a8b591469154a59bac76f6a19
J-Blackburn <J-Blackburn@users.noreply.github.com>
2018-04-23 21:07:09 -0400

    Create Gate.h

---
+++ Gate.h
@@ -0,0 +1,21 @@
+#ifndef GATE
+#define GATE
+#endif
+#pragma once
+
+class Wire;
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Gate {
+	public:
+	Gate(string tp, Wire *in1, Wire *in2, Wire *out, int dl);
+	bool compute(); //figure output from inputs and gate type
+	int getDelay();
+	private:
+	string type;
+	Wire *input1,*input2,*output;
+	int delay;
+};
