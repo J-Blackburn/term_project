35b8b8e11a43e55a89d851b0a34f71b5c22cecf5
J-Blackburn <J-Blackburn@users.noreply.github.com>
2018-04-27 01:02:46 -0400

    Create Print_function.cpp
    This isn't a full example, it just has the function to print the histories.

---
+++ Print_function.cpp
@@ -0,0 +1,15 @@
+//inplemented via command printHistories(&wires); given a vector of wire pointers called 'wires'
+void printHistories(vector<Wire*> *wires) {
+	
+for(int i = 0; i < wires->size(); i++) {
+	cout << (wires->at(i))->getHistory() << "  ";
+	string str = (wires->at(i))->getHistory();
+	while(str.find('0') < 100) {
+		str.replace(str.find('0'),1,"_");
+	}
+	while(str.find('1') < 100) {
+		str.replace(str.find('1'),1,"-");
+	}
+	cout << str << endl;
+	}
+}
